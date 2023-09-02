715cc080fb7d6d9ac5ca51e9fbc5e7b3c5cdbe42
RusJJ <mctesting1@yandex.ru>
2023-09-02 15:24:07 +0300

    Initial commit
    Works but wrong!

---
+++ README.md
@@ -0,0 +1,22 @@
+[![AML Mod Compiler](https://github.com/AndroidModLoader/GTASA_FluffyClouds/actions/workflows/main.yml/badge.svg?branch=main)](https://github.com/AndroidModLoader/GTASA_FluffyClouds/actions/workflows/main.yml)
+
+### What is this mod?
+
+This mod is a port of TheArtemMaps' port which is a transfering of re3 (reverse engineered GTA 3) Clouds rendering code (forget to mention it, HUH?)
+
+So actually it's a re3 code...
+
+PC Version: https://github.com/TheArtemMaps/fluffyclouds-sa
+
+## Fluffy clouds
+
+Port of GTA3/GTA VC's fluffy clouds to SA. With highlight effect, and every original feature.
+
+![gallery122](https://github.com/TheArtemMaps/fluffyclouds-sa/assets/91487356/11a11b56-7a17-482f-ad58-ddb342b62919)
+![gallery121](https://github.com/TheArtemMaps/fluffyclouds-sa/assets/91487356/83fa6397-dc9c-43b4-9be9-44e8829c97ad)
+![gallery120](https://github.com/TheArtemMaps/fluffyclouds-sa/assets/91487356/c4ffd107-0ad2-4d44-873a-660f46cc1bcd)
+![gallery119](https://github.com/TheArtemMaps/fluffyclouds-sa/assets/91487356/eabd1665-f50a-4044-a93e-c736425d0d9e)
+![gallery118](https://github.com/TheArtemMaps/fluffyclouds-sa/assets/91487356/b669d9ed-d4d7-490a-98f7-376e53f4c2c9)
+![gallery124](https://github.com/TheArtemMaps/fluffyclouds-sa/assets/91487356/2e2a27f2-b5d3-48d7-9b91-a720bcc8abc7)
+![gallery123](https://github.com/TheArtemMaps/fluffyclouds-sa/assets/91487356/6f69ef23-e4d9-481e-8416-4ef2a1b788d3)
+![gallery125](https://cdn.discordapp.com/attachments/1146215394491773030/1146570571371528202/image.png)
---
+++ main.cpp
@@ -0,0 +1,248 @@
+#include <mod/amlmod.h>
+#include <mod/logger.h>
+#include <isautils.h>
+
+#ifdef AML32
+    #include "GTASA_STRUCTS.h"
+    #define BYVER(__for32, __for64) (__for32)
+#else
+    #include "GTASA_STRUCTS_210.h"
+    #define BYVER(__for32, __for64) (__for64)
+#endif
+#define sizeofA(__aVar)  ((int)(sizeof(__aVar)/sizeof(__aVar[0])))
+
+MYMOD(net.theartemmaps.rusjj.fluff, FluffyCloudsSA, 1.0, TheArtemMaps & RusJJ)
+BEGIN_DEPLIST()
+    ADD_DEPENDENCY_VER(net.rusjj.aml, 1.0.2.2)
+END_DEPLIST()
+
+uintptr_t pGTASA;
+void* hGTASA;
+
+#define MAX_FLUFFY_CLOUDS 37
+
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+//////      Game Variables
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+float *Foggyness, *ExtraSunnyness, *ms_cameraRoll, *CloudRotation, *SunScreenX, *SunScreenY, *CloudCoverage;
+unsigned int *IndividualRotation;
+CCamera *TheCamera;
+bool *SunBlockedByClouds;
+CColourSet *m_CurrentColours;
+RsGlobalType *RsGlobal;
+TextureDatabaseRuntime* FluffyCloudsTexdb;
+
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+//////      Game Functions
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+void (*RwRenderStateSet)(RwRenderState, void*);
+float (*GetATanOfXY)(float, float);
+bool (*CalcScreenCoors)(RwV3d const&, RwV3d*, float *, float *, bool, bool);
+void (*RenderBufferedOneXLUSprite_Rotate_2Colours)(float,float,float,float,float,unsigned char,unsigned char,unsigned char,unsigned char,unsigned char,unsigned char,float,float,float,float,unsigned char);
+void (*RenderBufferedOneXLUSprite_Rotate_Aspect)(float,float,float,float,float,unsigned char,unsigned char,unsigned char,short,float,float,unsigned char);
+void (*FlushSpriteBuffer)();
+TextureDatabaseRuntime* (*TextureDatabaseLoad)(const char*, bool, TextureDatabaseFormat);
+int (*GetEntry)(TextureDatabaseRuntime *,char const*, bool*);
+RwTexture* (*GetRWTexture)(TextureDatabaseRuntime *, int);
+bool (*CanSeeOutSideFromCurrArea)();
+void (*InitSpriteBuffer)();
+
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+//////      Variables
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+RwTexture* gpCloudTex[5];
+bool CloudOnScreen[MAX_FLUFFY_CLOUDS];
+
+float CoorsOffsetX[MAX_FLUFFY_CLOUDS] = {
+    0.0f, 60.0f, 72.0f, 48.0f, 21.0f, 12.0f,
+    9.0f, -3.0f, -8.4f, -18.0f, -15.0f, -36.0f,
+    -40.0f, -48.0f, -60.0f, -24.0f, 100.0f, 100.0f,
+    100.0f, 100.0f, 100.0f, 100.0f, 100.0f, 100.0f,
+    100.0f, 100.0f, -30.0f, -20.0f, 10.0f, 30.0f,
+    0.0f, -100.0f, -100.0f, -100.0f, -100.0f, -100.0f, -100.0f
+};
+float CoorsOffsetY[MAX_FLUFFY_CLOUDS] = {
+    100.0f, 100.0f, 100.0f, 100.0f, 100.0f, 100.0f,
+    100.0f, 100.0f, 100.0f, 100.0f, 100.0f, 100.0f,
+    100.0f, 100.0f, 100.0f, 100.0f, -30.0f, 10.0f,
+    -25.0f, -5.0f, 28.0f, -10.0f, 10.0f, 0.0f,
+    15.0f, 40.0f, -100.0f, -100.0f, -100.0f, -100.0f,
+    -100.0f, -40.0f, -20.0f, 0.0f, 10.0f, 30.0f, 35.0f
+};
+float CoorsOffsetZ[MAX_FLUFFY_CLOUDS] = {
+    2.0f, 1.0f, 0.0f, 0.3f, 0.7f, 1.4f,
+    1.7f, 0.24f, 0.7f, 1.3f, 1.6f, 1.0f,
+    1.2f, 0.3f, 0.7f, 1.4f, 0.0f, 0.1f,
+    0.5f, 0.4f, 0.55f, 0.75f, 1.0f, 1.4f,
+    1.7f, 2.0f, 2.0f, 2.3f, 1.9f, 2.4f,
+    2.0f, 2.0f, 1.5f, 1.2f, 1.7f, 1.5f, 2.1f
+};
+
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+//////      Functions
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+inline float SQR(float v) { return v*v; }
+inline RwTexture* GetTextureFromTexDB(TextureDatabaseRuntime* texdb, const char* name)
+{
+    bool hasSiblings;
+    return GetRWTexture(texdb, GetEntry(texdb, name, &hasSiblings));
+}
+inline void RenderFluffyClouds()
+{
+    *SunBlockedByClouds = false;
+    int fluffyalpha = 160 * (1.0f - fmax(*Foggyness, *ExtraSunnyness));
+    if(fluffyalpha != 0)
+    {
+        InitSpriteBuffer();
+
+        float sundist, hilight;
+        float szx, szy;
+        RwV3d screenpos, worldpos;
+
+        CVector campos = TheCamera->GetPosition();
+        float rot_sin = sinf(*CloudRotation);
+        float rot_cos = cosf(*CloudRotation);
+
+        RwRenderStateSet(rwRENDERSTATESRCBLEND, (void*)rwBLENDSRCALPHA);
+        RwRenderStateSet(rwRENDERSTATEDESTBLEND, (void*)rwBLENDINVSRCALPHA);
+        RwRenderStateSet(rwRENDERSTATETEXTURERASTER, gpCloudTex[3]->raster);
+        for(int i = 0; i < MAX_FLUFFY_CLOUDS; ++i)
+        {
+            RwV3d pos = { 2.0f * CoorsOffsetX[i], 2.0f * CoorsOffsetY[i], 40.0f * CoorsOffsetZ[i] + 40.0f };
+            worldpos.x = pos.x * rot_cos + pos.y * rot_sin + campos.x;
+            worldpos.y = pos.x * rot_sin - pos.y * rot_cos + campos.y;
+            worldpos.z = pos.z;
+
+            if (CalcScreenCoors(worldpos, &screenpos, &szx, &szy, false, false))
+            {
+                sundist = sqrtf(SQR(screenpos.x - *SunScreenX) + SQR(screenpos.y - *SunScreenY));
+                int tr = 0;
+                int tg = 0;
+                int tb = 0;
+                int br = m_CurrentColours->fluffycloudr;
+                int bg = m_CurrentColours->fluffycloudg;
+                int bb = m_CurrentColours->fluffycloudb;
+
+                int distLimit = (3 * RsGlobal->maximumWidth) / 4;
+                if (sundist < distLimit)
+                {
+                    hilight = (1.0f - fmax(*Foggyness, *CloudCoverage)) * (1.0f - sundist / (float)distLimit);
+                    tr = tr * (1.0f - hilight) + 255 * hilight;
+                    tg = tg * (1.0f - hilight) + 150 * hilight;
+                    tb = tb * (1.0f - hilight) + 150 * hilight;
+                    br = br * (1.0f - hilight) + 255 * hilight;
+                    bg = bg * (1.0f - hilight) + 150 * hilight;
+                    bb = bb * (1.0f - hilight) + 150 * hilight;
+
+                    if (sundist < RsGlobal->maximumWidth / 10) *SunBlockedByClouds = true;
+                }
+                else
+                {
+                    hilight = 0.0f;
+                }
+                CloudOnScreen[i] = true;
+                RenderBufferedOneXLUSprite_Rotate_2Colours(screenpos.x, screenpos.y, screenpos.z, szx * 55.0f, szy * 55.0f, tr, tg, tb, br, bg, bb, 0.0f, -1.0f,
+                                                           1.0f / screenpos.z, (uint16_t)*IndividualRotation / 65336.0f * 6.28f + *ms_cameraRoll, fluffyalpha);
+            }
+            else
+            {
+                CloudOnScreen[i] = false;
+            }
+        }
+        FlushSpriteBuffer();
+
+        RwRenderStateSet(rwRENDERSTATESRCBLEND, (void*)rwBLENDONE);
+        RwRenderStateSet(rwRENDERSTATEDESTBLEND, (void*)rwBLENDONE);
+        RwRenderStateSet(rwRENDERSTATETEXTURERASTER, gpCloudTex[4]->raster);
+        for(int i = 0; i < MAX_FLUFFY_CLOUDS; ++i)
+        {
+            if(!CloudOnScreen[i]) continue;
+
+            RwV3d pos = { 2.0f * CoorsOffsetX[i], 2.0f * CoorsOffsetY[i], 40.0f * CoorsOffsetZ[i] + 40.0f };
+            worldpos.x = pos.x * rot_cos + pos.y * rot_sin + campos.x;
+            worldpos.y = pos.x * rot_sin - pos.y * rot_cos + campos.y;
+            worldpos.z = pos.z;
+
+            if (CalcScreenCoors(worldpos, &screenpos, &szx, &szy, false, false))
+            {
+                if (sundist < RsGlobal->maximumWidth / 3)
+                {
+                    RenderBufferedOneXLUSprite_Rotate_Aspect(screenpos.x, screenpos.y, screenpos.z, szx * 30.0f, szy * 30.0f, 200 * hilight, 0, 0, 255, 1.0f / screenpos.z,
+                                                             1.7f - GetATanOfXY(screenpos.x - *SunScreenX, screenpos.y - *SunScreenY) + *ms_cameraRoll, 255);
+                }
+            }
+        }
+        FlushSpriteBuffer();
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+//////      Hooks
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+DECL_HOOK(bool, GameInit3, void* data)
+{
+    bool ret = GameInit3(data);
+    FluffyCloudsTexdb = TextureDatabaseLoad("fluffyclouds", false, DF_Default);
+    if(FluffyCloudsTexdb)
+    {
+        gpCloudTex[3] = GetTextureFromTexDB(FluffyCloudsTexdb, "cloudmasked");
+        gpCloudTex[4] = GetTextureFromTexDB(FluffyCloudsTexdb, "cloudhilit");
+    }
+    return ret;
+}
+DECL_HOOKv(RenderEffects)
+{
+    if (CanSeeOutSideFromCurrArea()) RenderFluffyClouds();
+    RenderEffects();
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+//////      Main
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+extern "C" void OnAllModsLoaded()
+{
+    logger->SetTag("FluffyCloudsSA");
+
+    pGTASA = aml->GetLib("libGTASA.so");
+    hGTASA = aml->GetLibHandle("libGTASA.so");
+    
+    SET_TO(Foggyness, aml->GetSym(hGTASA, "_ZN8CWeather9FoggynessE"));
+    SET_TO(ExtraSunnyness, aml->GetSym(hGTASA, "_ZN8CWeather14ExtraSunnynessE"));
+    SET_TO(ms_cameraRoll, aml->GetSym(hGTASA, "_ZN7CClouds13ms_cameraRollE"));
+    SET_TO(CloudRotation, aml->GetSym(hGTASA, "_ZN7CClouds13CloudRotationE"));
+    SET_TO(SunScreenX, aml->GetSym(hGTASA, "_ZN8CCoronas10SunScreenXE"));
+    SET_TO(SunScreenY, aml->GetSym(hGTASA, "_ZN8CCoronas10SunScreenYE"));
+    SET_TO(CloudCoverage, aml->GetSym(hGTASA, "_ZN8CWeather13CloudCoverageE"));
+    SET_TO(IndividualRotation, aml->GetSym(hGTASA, "_ZN7CClouds18IndividualRotationE"));
+    SET_TO(TheCamera, aml->GetSym(hGTASA, "TheCamera"));
+    SET_TO(SunBlockedByClouds, aml->GetSym(hGTASA, "_ZN8CCoronas18SunBlockedByCloudsE"));
+    SET_TO(m_CurrentColours, aml->GetSym(hGTASA, "_ZN10CTimeCycle16m_CurrentColoursE"));
+    SET_TO(RsGlobal, aml->GetSym(hGTASA, "RsGlobal"));
+
+    SET_TO(RwRenderStateSet, aml->GetSym(hGTASA, "_Z16RwRenderStateSet13RwRenderStatePv"));
+    SET_TO(GetATanOfXY, aml->GetSym(hGTASA, "_ZN8CGeneral11GetATanOfXYEff"));
+    SET_TO(CalcScreenCoors, aml->GetSym(hGTASA, "_ZN7CSprite15CalcScreenCoorsERK5RwV3dPS0_PfS4_bb"));
+    SET_TO(RenderBufferedOneXLUSprite_Rotate_2Colours, aml->GetSym(hGTASA, "_ZN7CSprite42RenderBufferedOneXLUSprite_Rotate_2ColoursEfffffhhhhhhffffh"));
+    SET_TO(RenderBufferedOneXLUSprite_Rotate_Aspect, aml->GetSym(hGTASA, "_ZN7CSprite40RenderBufferedOneXLUSprite_Rotate_AspectEfffffhhhsffh"));
+    SET_TO(FlushSpriteBuffer, aml->GetSym(hGTASA, "_ZN7CSprite17FlushSpriteBufferEv"));
+    SET_TO(TextureDatabaseLoad, aml->GetSym(hGTASA, "_ZN22TextureDatabaseRuntime4LoadEPKcb21TextureDatabaseFormat"));
+    SET_TO(GetEntry, aml->GetSym(hGTASA, "_ZN22TextureDatabaseRuntime8GetEntryEPKcRb"));
+    SET_TO(GetRWTexture, aml->GetSym(hGTASA, "_ZN22TextureDatabaseRuntime12GetRWTextureEi"));
+    SET_TO(CanSeeOutSideFromCurrArea, aml->GetSym(hGTASA, "_ZN5CGame25CanSeeOutSideFromCurrAreaEv"));
+    SET_TO(InitSpriteBuffer, aml->GetSym(hGTASA, "_ZN7CSprite16InitSpriteBufferEv"));
+
+    HOOKPLT(GameInit3, pGTASA + BYVER(0x6742F0, 0x8470F0));
+    HOOKPLT(RenderEffects, pGTASA + BYVER(0x670BD4, 0x8452B0));
+}
